a84fdc280a624cfff51b0c00998e05ca1eb2cfef
Alex <alex@alexvarney.com>
2016-03-16 19:46:29 -0400

    test

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ file_extentions = ['jpg', 'jpeg', 'png', 'bmp', 'gif']
 url_characters = "abcdefghjkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 upload_directory = "images"
 default_url_length = 8
-app_path = 'http://' + os.environ.get('APP_URL', "127.0.0.1:5000")
+app_path = os.environ.get('APP_URL', "127.0.0.1:5000")
