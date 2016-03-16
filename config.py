71b74abc8b5e5dcd4800766af306bffa6afd5fc4
Alex <alex@alexvarney.com>
2016-03-16 19:42:57 -0400

    updated config values to check for an 'APP_URL' environment value'

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
+
 file_extentions = ['jpg', 'jpeg', 'png', 'bmp', 'gif']
 url_characters = "abcdefghjkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 upload_directory = "images"
 default_url_length = 8
-app_path = "http://127.0.0.1:5000"
+app_path = 'http://' + os.environ.get('APP_URL', "127.0.0.1:5000")
