3df49774230075c1fde15c4a5d20caf08e72b3b4
Alex <alex@alexvarney.com>
2016-03-23 14:20:55 -0400

    added AWS config

--- config.py
+++ config.py
@@ -4,8 +4,13 @@ file_extentions = ['jpg', 'jpeg', 'png', 'bmp', 'gif']
 url_characters = "abcdefghjkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789"
 upload_directory = "images"
 default_url_length = 8
-app_path = os.environ.get('APP_URL', "127.0.0.1:5000")
-app_password = os.environ.get('APP_PASS', "password")
-app_username = os.environ.get('APP_USER', "admin")
-aws_access_key = os.environ.get('AWS_ACCESS_KEY', "<key_here>")
-aws_secret_key = os.environ.get('AWS_SECRET_KEY', "<key_here>")
+app_path = os.environ.get('APP_URL', '127.0.0.1:5000')
+app_password = os.environ.get('APP_PASS', 'password')
+app_username = os.environ.get('APP_USER', 'admin')
+
+aws_access_key = os.environ.get('AWS_ACCESS_KEY')
+aws_secret_key = os.environ.get('AWS_SECRET_KEY')
+
+aws_s3_endpoint = 's3.amazonaws.com'
+aws_s3_bucket_id = 'varney-me-images'
+aws_s3_bucket_path = 'images'
