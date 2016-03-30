068dd427844b5081a9397eaf2c9049640abfc68f
Alex <alex@varney.me>
2016-03-30 18:00:54 -0400

    added temporary imagemodel class

---
+++ models.py
@@ -0,0 +1,5 @@
+class ImageModel:
+    def __init__(self, file_url, display_url, timestamp=0):
+        self.file_url = file_url
+        self.display_url = display_url
+        self.timestamp = timestamp
